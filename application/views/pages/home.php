a8b83d941d756fa497755212079545c2b1bc6f41
Elias Verstappen <eliasverstappen@msn.com>
2013-02-11 11:52:37 +0100

    testing

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="span12">
-                Derp
+                Derpieeee
             </div>
         </div>
     </div>
